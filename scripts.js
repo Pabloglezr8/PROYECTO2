69b7c539afab5a5a5679facae18bec2cef368deb
Pabloglezr8 <pabligoru99@gmail.com>
2023-09-21 13:26:27 +0200

    Se hace el script y el diseño de la pagina

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const convertirGrados(){
+const celsius = parseFloat(document.getElementById("celsius").value);
+
+const fahrenheit = (celsius * 9/5) + 32;
+
+document.getElementById("fahrenheitResultado").textContent = fahrenheit.toFixed(2);
+
+}
